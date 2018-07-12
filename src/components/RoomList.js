b59628db075d0a19d202ec2a9c9d31856e4483d7
Massimiliano Tavanti <max.tavanti@gmail.com>
2018-07-12 15:16:25 +0200

    commit university logo

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,12 +1,17 @@
 import React from 'react'
 
+import cuLogo from './../images/cu_logo.png'
+
 class RoomList extends React.Component {
     render () {
         const orderedRooms = [...this.props.rooms].sort((a, b) => a.id > b.id)
         return (
             <div className="rooms-list">
+                <div className="rooms-list-logo-wrap">
+                    <img className="rooms-list-logo" src={cuLogo}/>
+                </div>
                 <ul>
-                <h3>Your rooms:</h3>
+                    <h3>Your rooms:</h3>
                     {orderedRooms.map(room => {
                         const active = room.id === this.props.roomId ? 'active' : '';
                         return (
